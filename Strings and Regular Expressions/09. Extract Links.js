d29e6882437b65597a04d8d14df120943b10bca1
Nelov87 <nelov87@gmail.com>
2018-06-07 16:44:14 +0300

    update

--- Strings and Regular Expressions/09. Extract Links.js
+++ Strings and Regular Expressions/09. Extract Links.js
@@ -1,3 +1,12 @@
-function extractLinks() {
-    
+function extractLinks(text) {
+    let links = [];
+    let regex = /www\.[A-Za-z0-9\-]+(\.[a-z]+)+/g;
+
+    for(let sentence of text) {
+        let match = null;
+        let index = 0;
+        while(match = regex.exec(sentence)) {
+            console.log(match[0]);
+        }
+    }
 }
